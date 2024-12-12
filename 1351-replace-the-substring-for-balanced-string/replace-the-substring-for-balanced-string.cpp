3181fd7aa7f4c4b2a2e75b97f91cd1e9f04081c9
Ansh Soni <anshs.dev@gmail.com>
2024-12-12 11:43:01 +0530

    Time: 15 ms (74.72%) | Memory: 10.7 MB (15.06%) - LeetSync

--- 1351-replace-the-substring-for-balanced-string/replace-the-substring-for-balanced-string.cpp
+++ 1351-replace-the-substring-for-balanced-string/replace-the-substring-for-balanced-string.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int balancedString(string s) {
-        int left=0,target=(s.length())/4,right=0;
-        int min_len=INT_MAX;
         unordered_map<char,int>m;
         for(char x:s)
         m[x]++;
+        int target=s.length()/4;
         int flag=0;
         for(auto &x:m){
             if(x.second!=target){
@@ -15,17 +14,16 @@ public:
         }
         if(flag==0)
         return 0;
+        int left=0,right=0,min_len=INT_MAX;
         while(right<s.length()){
-        m[s[right]]--;
-        while(left<=right && m['Q']<=target && m['W']<=target && m['E']<=target && m['R']<=target){
-           min_len=min(min_len,right-left+1);
-            m[s[left]]++;
-            left++;
+            m[s[right]]--;
+            while(left<=right && m['Q']<=target && m['W']<=target && m['E']<=target && m['R']<=target){
+                min_len=min(min_len,right-left+1);
+                m[s[left]]++;
+                left++;
+            }
+            right++;
         }
-        right++;
-        }
-
-        
         return min_len;
     }
 };
