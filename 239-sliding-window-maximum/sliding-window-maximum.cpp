bc99f24290b7f87a8d914b5dd07bb6af6a5ab37c
Ansh Soni <anshs.dev@gmail.com>
2024-12-14 14:47:14 +0530

    Time: 59 ms (27.71%) | Memory: 153.3 MB (16.67%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        vector<int>result;
         priority_queue<pair<int,int>>maxHeap;
-        for(int i=0;i<k;i++)
-        maxHeap.push({nums[i],i});
+        for(int i=0;i<k;i++){
+            maxHeap.push({nums[i],i});
+        }
+        vector<int>result;
         result.push_back(maxHeap.top().first);
         for(int i=k;i<nums.size();i++){
             maxHeap.push({nums[i],i});
