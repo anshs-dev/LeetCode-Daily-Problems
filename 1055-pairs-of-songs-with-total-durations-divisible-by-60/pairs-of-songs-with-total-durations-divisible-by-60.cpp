cb161da482705a764d8510d0dccec50cb52d0f67
Ansh Soni <anshs.dev@gmail.com>
2024-12-27 07:35:29 +0530

    Time: 10 ms (22.66%) | Memory: 32.1 MB (12.22%) - LeetSync

---
+++ 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numPairsDivisibleBy60(vector<int>& time) {
+        int count=0;
+        unordered_map<int,int>m;
+        for(int x:time){
+            int remainder=x%60;
+            int complement=(60-remainder)%60;
+            if(m.count(complement)){
+                count+=m[complement];
+            }
+            m[remainder]++;
+        }
+        return count;
+    }
+};
