74a176bdb359851c33e3759224cf1db24eb431f0
Ansh Soni <anshs.dev@gmail.com>
2025-01-06 18:22:43 +0530

    Time: 1 ms (43.84%) | Memory: 10.6 MB (39.22%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,19 +1,10 @@
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>>v;
-        vector<int>temp=nums;
-        do{
-            v.push_back(nums);
-        }
-        while(next_permutation(nums.begin(),nums.end()));
         sort(nums.begin(),nums.end());
-        do{
-            if(nums==temp)
-            break;
-            else
-            v.push_back(nums);
-        }
+        vector<vector<int>>v;
+        do
+        v.push_back(nums);
         while(next_permutation(nums.begin(),nums.end()));
         return v;
     }
