04697cde46210aa37c9d85f8b4f9b6b400ee83ec
Ansh Soni <anshs.dev@gmail.com>
2025-01-11 11:27:45 +0530

    Time: 10 ms (43.36%) | Memory: 15 MB (33.92%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.cpp
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     bool canConstruct(string s, int k) {
-        if(k>s.length())
-        return false;
         unordered_map<char,int>m;
+        if(s.length()<k)
+        return false;
         for(char x:s)
         m[x]++;
         int odd=0;
         for(auto &x:m){
-        if(x.second%2!=0)
-        odd++;
+            if(x.second%2!=0)
+            odd++;
         }
-        return k>=odd;
+    return k>=odd;
     }
 };
