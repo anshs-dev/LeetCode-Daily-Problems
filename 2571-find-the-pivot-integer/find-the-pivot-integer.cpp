bd26537d3abc38304a93f074165985f121e2b9b4
Ansh Soni <anshs.dev@gmail.com>
2025-01-13 09:56:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (34.03%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        vector<int>prefix1(n,0);
-        vector<int>prefix2(n,0);
-        prefix1[0]=1;
-        for(int i=2;i<=n;i++){
-            prefix1[i-1]=i+prefix1[i-2];
-        }
-        prefix2[n-1]=n;
-        for(int i=n-2;i>=0;i--){
-        prefix2[i]=i+1+prefix2[i+1];
-        }
-        for(int i=0;i<n;i++){
-            cout<<prefix2[i]<<" ";
-            if(prefix1[i]==prefix2[i])
-            return i+1;
-        }
-        return -1;
+        int sum = n * (n + 1) / 2;
+        double a = sqrt(sum);
+
+        if (a - ceil(a) == 0)
+            return int(a);
+        else
+            return -1;
     }
 };
