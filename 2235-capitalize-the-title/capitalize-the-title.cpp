3f1217af9d7ad88c7affd454d955d68eaf396dce
Ansh Soni <anshs.dev@gmail.com>
2025-01-28 01:45:25 +0530

    Time: 3 ms (15.83%) | Memory: 9.6 MB (14.63%) - LeetSync

---
+++ 2235-capitalize-the-title/capitalize-the-title.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string capitalizeTitle(string title) {
+        stringstream ss(title);
+        vector<string>result;
+        string word="";
+        while(ss>>word) result.push_back(word);
+        for(string &x:result){
+            cout<<x;
+            if(x.length()<=2){
+                for(int i=0;i<x.length();i++)
+                x[i]=tolower(x[i]);
+            }
+            else{
+                x[0]=toupper(x[0]);
+                for(int i=1;i<x.length();i++){
+                    x[i]=tolower(x[i]);
+                }
+            }
+        }
+        string result1="";
+        for(string x:result){
+            result1+=x;
+            result1+=" ";
+        }
+        result1.pop_back();
+        return result1;
+    }
+};
