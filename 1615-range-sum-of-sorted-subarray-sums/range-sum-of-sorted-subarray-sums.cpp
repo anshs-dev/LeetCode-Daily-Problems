68a1616134980168a8852845f219e20e24bc9f84
Ansh Soni <anshs.dev@gmail.com>
2025-02-02 11:27:40 +0530

    Time: 92 ms (41.34%) | Memory: 26.9 MB (66.48%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     int rangeSum(vector<int>& nums, int n, int left, int right) {
-        multiset<int>m;
+        vector<int>result;
         const int mod=1000000007;
         for(int i=0;i<nums.size();i++){
-            long long sum=0;
+            int sum=0;
             for(int j=i;j<nums.size();j++){
                 sum+=nums[j];
-                m.insert(sum);
+                sum=sum%mod;
+                result.push_back(sum);
             }
         }
         left--;
         int sum=0;
-        for(int x:m){
-            if(left==0 && right==0) break;
-            if(left==0){
-                sum+=x;
-                sum=sum%mod;
-                right--;
-            }
-            else{
-                left--;
-                right--;
-            }
+        sort(result.begin(),result.end());
+        for(int i=left;i<right;i++){
+            sum+=result[i];
+            sum=sum%mod;
         }
         return sum;
     }
