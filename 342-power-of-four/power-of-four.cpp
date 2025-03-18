cd4cadb94d39252d14699c6a5d637a10c2737313
Ansh Soni <anshs.dev@gmail.com>
2025-03-18 18:35:04 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (53.00%) - LeetSync

--- 342-power-of-four/power-of-four.cpp
+++ 342-power-of-four/power-of-four.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool isPowerOfFour(int n) {
-        return n>0 && (n & (n-1))==0 && (n-1)%3==0;
+    bool isPowerOfFour(long long n) {
+      return  n>0 && __builtin_popcount(n)==1 && (n-1)%3==0;
     }
 };
