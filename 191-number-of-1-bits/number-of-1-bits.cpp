8a7735e2e0f29c4a4ae8ee937aab11f38eacab4c
Ansh Soni <anshs.dev@gmail.com>
2025-03-18 18:21:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (80.26%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
     int hammingWeight(int n) {
-        int count=1;
-        while(n!=1){
-        if(n%2!=0){
-            count++;
-            n/=2;
-        }
-        else{
-        n/=2;
-        }
-        }
-        return count;
+        return __builtin_popcount(n);
     }
 };
