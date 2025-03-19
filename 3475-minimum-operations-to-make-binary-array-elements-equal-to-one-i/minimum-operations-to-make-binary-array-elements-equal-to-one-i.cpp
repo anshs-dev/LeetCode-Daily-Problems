32e27d951aa28000735a361313df94750ec4e642
Ansh Soni <anshs.dev@gmail.com>
2025-03-19 14:32:36 +0530

    Time: 15 ms (16.43%) | Memory: 140 MB (47.55%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        int swaps=0;
-        for(int i=0;i<nums.size()-2;i++){
-            if(nums[i]==0){
-                nums[i]=1;
-                if(nums[i+1]==1)
-                nums[i+1]=0;
-                else if(nums[i+1]==0)
-                nums[i+1]=1;
-                if(nums[i+2]==1)
-                nums[i+2]=0;
-                else if(nums[i+2]==0)
-                nums[i+2]=1;
-                swaps++;
-            }
+       int count=0;
+       for(int i=0;i<nums.size()-2;i++){
+        if(nums[i]==0){
+            nums[i]=1;
+            nums[i+1]=(nums[i+1]==0)?1:0;
+            nums[i+2]=(nums[i+2]==0)?1:0;
+            count++;
         }
-        for(int x:nums){
-            if(x==0) return -1;
-        }
-        return swaps;
+       }
+       if(nums[nums.size()-1]==0 || nums[nums.size()-2]==0) return -1;
+       return count;
     }
 };
