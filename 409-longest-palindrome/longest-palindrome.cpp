52804afe1e09c28fde107fb804910e37b6ac8ec4
Ansh Soni <anshs.dev@gmail.com>
2025-10-11 14:47:53 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (36.48%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-      unordered_map<char,int>m;
-      for(char x:s)
-      m[x]++;
-      int len=0;
-      int flag=0;
-      for(auto &x:m){
-        if(x.second%2==0){
-            len+=x.second;
-        }
+        int res=0;
+        bool odd_is_present=false;
+        unordered_map<char,int>m;
+        for(char x:s) m[x]++;
+        for(auto x:m) if(x.second%2==0) res+=x.second;
         else{
-          len+=(x.second-1);
-          flag=1;
+            odd_is_present=true;
+            res+=x.second-1;
         }
-      }
-      if(flag==1)
-      return len+1;
-      else
-      return len;
+        return odd_is_present?res+1:res;
     }
 };
