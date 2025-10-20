e3c932459a26b546d8b2f6fefa5459fc9f537b6d
Ansh Soni <anshs.dev@gmail.com>
2025-10-20 09:53:49 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (74.44%) - LeetSync

--- 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.cpp
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.cpp
@@ -1,13 +1,8 @@
 class Solution {
 public:
     int finalValueAfterOperations(vector<string>& operations) {
-        int n=0;
-        for(string x:operations){
-            if(x[0]=='-' || x[2]=='-')
-            n--;
-            else
-            n++;
-        }
-        return n;
+        int count=0;
+        for(auto x:operations) count=x[1]=='+'?count+1:count-1;
+        return count;
     }
 };
