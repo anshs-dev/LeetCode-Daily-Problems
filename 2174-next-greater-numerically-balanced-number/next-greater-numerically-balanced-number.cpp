ed118b8056524588169bfeb94ada6848b340809d
Ansh Soni <anshs.dev@gmail.com>
2025-10-24 22:06:00 +0530

    Time: 30 ms (67.84%) | Memory: 9 MB (76.38%) - LeetSync

---
+++ 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int nextBeautifulNumber(int n) {
+        if (n == 0) return 1;
+        int minNumLen = log10(n) + 1;
+        for (int numLen = minNumLen; numLen <= minNumLen + 1; ++numLen) {
+            vector<int> all;
+            unordered_map<int, int> counter;
+            backtracking(0, numLen, 0, counter, all);
+            for (int num : all)
+                if (num > n) return num;
+        }
+        return -1;
+    }
+    void backtracking(int i, int numLen, int curNum, unordered_map<int, int>& counter, vector<int>& out) {
+        if (i == numLen) {
+            bool isBalanceNumber = true;
+            for (auto& [d, freq] : counter)
+                if (freq > 0 && d != freq)
+                    isBalanceNumber = false;
+            if (isBalanceNumber) 
+                out.push_back(curNum);
+            return;
+        }
+        for (int d = 1; d <= numLen; ++d) {
+            if (counter[d] >= d) continue; // Prune if number of occurrences of `d` is greater than `d`
+            if (counter[d] + (numLen - i) < d) continue; // Prune if not enough number of occurrences of `d`
+            counter[d] += 1;
+            backtracking(i + 1, numLen, curNum * 10 + d, counter, out);
+            counter[d] -= 1;
+        }
+    }
+};
