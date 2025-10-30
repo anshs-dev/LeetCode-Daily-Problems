abe6cf782382be48b5e332ef203a48c66926c075
Ansh Soni <anshs.dev@gmail.com>
2025-10-30 11:29:43 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (47.95%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -4,6 +4,8 @@ public:
         if(!p && !q) return true;
         if(!p || !q) return false;
         if(p->val!=q->val) return false;
-        return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+        bool left=isSameTree(p->left,q->left);
+        bool right=isSameTree(p->right,q->right);
+        return left && right;
     }
 };
