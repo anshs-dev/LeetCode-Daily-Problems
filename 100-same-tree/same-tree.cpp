09964c07249278f24122f451ac9b33f5afbcd680
Ansh Soni <anshs.dev@gmail.com>
2025-10-30 11:26:09 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (47.95%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -1,12 +1,8 @@
 class Solution {
 public:
-    Solution(){
-        ios::sync_with_stdio(false);
-        cin.tie(nullptr);
-    }
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(!p && !q) return true;
-        if(!p || !q) return false;
+        if(p==nullptr && q==nullptr) return true;
+        if(p==nullptr || q==nullptr) return false;
         if(p->val!=q->val) return false;
         return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
     }
