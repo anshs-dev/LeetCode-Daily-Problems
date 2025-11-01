d1c552ea2d06827fe665359394416181acf16d14
Ansh Soni <anshs.dev@gmail.com>
2025-11-01 19:20:09 +0530

    Time: 95 ms (71.38%) | Memory: 261.4 MB (90.17%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
-        set<int> s(nums.begin(), nums.end());
-        ListNode* dummy = new ListNode(0, head);
-        ListNode* current = dummy;
-        while(current->next!=nullptr){
-            if(s.find(current->next->val)!=s.end()) {
-                current->next=current->next->next;
-            }else{
-                current=current->next;
+        //We are going to traverse through the whole linked list and if the current node value
+        //is present in the nums, we will delete it...
+        //so for fast searching we will use set to check whether the element was present 
+        //or not
+        unordered_set<int>st(nums.begin(),nums.end());
+        ListNode* temp=head,*prev=nullptr;
+        while(temp!=nullptr){
+            if(st.find(temp->val)!=st.end()){
+                if(temp->next){
+                //we write these two lines whenever we want to delete a node
+                temp->val=temp->next->val;
+                temp->next=temp->next->next;
+                }
+                else{
+    //since we cant delete the last node, we have to take a previous pointer so if we encounter
+    //a last node and we have to delete it, we will set next of prev to nullptr.
+                    prev->next=nullptr;
+                    temp=temp->next;
+                }
+            }
+            else{
+                prev=temp;
+                temp=temp->next;
             }
         }
-        return dummy->next;
+        return head;
     }
 };
