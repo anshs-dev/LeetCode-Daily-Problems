9146e1c885f114903292610598ff89d28ad9e998
Ansh Soni <anshs.dev@gmail.com>
2025-11-09 18:31:46 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (71.74%) - LeetSync

--- 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.cpp
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int countOperations(int num1, int num2) {
-        int count=0;
+        int steps=0;
         while(num1!=0 && num2!=0){
-            if(num1>num2)
-            num1=num1-num2;
-            else
-            num2-=num1;
-            count++;
+            if(num1>num2) num1-=num2;
+            else num2-=num1;
+            steps++;
         }
-        return count;
+        return steps;
     }
 };
