79a488154ba0bc9908760c7158b69bb383d12fa3
Ansh Soni <anshs.dev@gmail.com>
2025-11-21 06:51:00 +0530

    Time: 2 ms (58.20%) | Memory: 10.9 MB (73.65%) - LeetSync

--- 1130-last-stone-weight-ii/last-stone-weight-ii.cpp
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.cpp
@@ -2,11 +2,17 @@ class Solution {
 public:
     int lastStoneWeightII(vector<int>& stones) {
         int sum=accumulate(stones.begin(),stones.end(),0);
-        int target=sum/2;
-        vector<bool>dp(sum,false);
-        dp[0]=true;
-        for(int w:stones) for(int i=sum;i>=w;i--) dp[i]=dp[i] || dp[i-w];
-        for(int i=target;i>=0;i--) if(dp[i]) return sum-2*i;
+        int target=sum/2,m=stones.size();
+        vector<vector<bool>>dp(m+1,vector<bool>(target+1,false));
+        dp[0][0]=true;
+        for(int i=1;i<=m;i++){
+            for(int j=0;j<=target;j++){ 
+                dp[i][j]=dp[i-1][j];
+                if(j>=stones[i-1])
+                dp[i][j]=dp[i][j] || dp[i-1][j-stones[i-1]];
+            }
+        }
+        for(int j=target;j>=0;j--) if(dp[m][j]) return sum-2*j;
         return 0;
     }
 };
