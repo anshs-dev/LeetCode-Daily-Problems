ba9dc1721eaf10c17a2b85e783d6268d81086916
Ansh Soni <anshs.dev@gmail.com>
2026-02-02 07:36:04 +0530

    Time: 27 ms (5.85%) | Memory: 48.3 MB (11.51%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.rbegin(),g.rend());
+        sort(s.rbegin(),s.rend());
+        int count=0,j=0;
+        for(int i=0;i<g.size();i++){
+            if(j<s.size() && g[i]<=s[j]){
+                count++;
+                j++;
+            }
+        }
+        return count;
+    }
+};
