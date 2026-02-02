295f51edeecaa2bd33162239d0ae6583ceeb8c12
Ansh Soni <anshs.dev@gmail.com>
2026-02-02 19:57:23 +0530

    Time: 43 ms (92.12%) | Memory: 13.8 MB (80.95%) - LeetSync

--- 474-ones-and-zeroes/ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/ones-and-zeroes.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     int findMaxForm(vector<string>& strs, int m, int n) {
-        auto &n0 = m, &n1 = n;
-        array<array<int, 101>, 101> dp{};
-        for (auto& str : strs) { // O (600 *100 *100) == 6e6
-            int c0 = ranges::count(str, '0');
-            int c1 = str.size() - c0;
-            // add this substr, only once
-            for (int i = m; i >= c0; --i)
-                for (int j = n; j >= c1; --j) {
-                    dp[i][j] = max(dp[i][j], 1 + dp[i - c0][j - c1]);
+        vector<pair<int,int>>v;
+        for(auto x:strs){
+            int zero=0,one=0;
+            for(char c:x) if(c=='0') zero++;
+            else one++;
+            v.push_back({zero,one});
+        }
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        for(auto x:v){
+            for(int i=m;i>=x.first;i--){
+                for(int j=n;j>=x.second;j--){
+                    dp[i][j]=max(dp[i][j],dp[i-x.first][j-x.second]+1);
                 }
+            }
         }
         return dp[m][n];
     }
