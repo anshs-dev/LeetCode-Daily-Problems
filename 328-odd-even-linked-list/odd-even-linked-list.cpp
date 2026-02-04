1c1c48f4eac8f7c839f3279ac35277466457f55b
Ansh Soni <anshs.dev@gmail.com>
2026-02-04 23:11:23 +0530

    Time: 0 ms (100.00%) | Memory: 15.4 MB (87.11%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -11,42 +11,16 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        struct ListNode *l1=NULL,*l2=NULL,*t1,*t2,*temp;
-        temp=head;
-        if(head==NULL)
+        if(!head || !head->next) return head;
+        ListNode *h2=head->next;
+        ListNode *t1=head, *t2=h2;
+        while(t2 && t2->next){
+            t1->next=t2->next;
+            t1=t1->next;
+            t2->next=t1->next;
+            t2=t2->next;
+        }
+        t1->next=h2;
         return head;
-        else{
-            int i=1;
-            while(temp!=NULL){
-            if(i%2!=0){
-                if(l1==NULL){
-                    l1=temp;
-                    t1=l1;
-                }
-                else{
-                    t1->next=temp;
-                    t1=t1->next;
-                }
-            }
-            else{
-                if(l2==NULL){
-                    l2=temp;
-                    t2=l2;
-                }
-                else{
-                    t2->next=temp;
-                    t2=t2->next;
-                }
-            }
-            temp=temp->next;
-            i++;
-            }
-        }
-        if(t1!=NULL){
-            t1->next=l2;
-        }
-        if(t2!=NULL)
-        t2->next=NULL;
-        return l1;
     }
 };
