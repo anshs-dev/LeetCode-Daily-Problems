6e9448fc03e1a6dc3a44129294f0ed8cc40084b8
Ansh Soni <anshs.dev@gmail.com>
2026-02-05 19:04:24 +0530

    Time: 3 ms (64.72%) | Memory: 11.5 MB (81.76%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,12 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-        vector<vector<int>>v;
-        sort(nums.begin(),nums.end());
-        do{
-            v.push_back(nums);
+    void dfs(vector<vector<int>> &grid,vector<int> &nums, vector<bool> &check,vector<int>& temp){
+        if(temp.size()==nums.size()){
+            grid.push_back(temp);
+            return;
         }
-        while(next_permutation(nums.begin(),nums.end()));
-        return v;
+        for(int i=0;i<nums.size();i++){
+            if(!check[i]){
+                check[i]=true;
+                temp.push_back(nums[i]);
+                dfs(grid,nums,check,temp);
+                check[i]=false;
+                temp.pop_back();
+                while(i<nums.size()-1 && nums[i]==nums[i+1]) i++;
+            }
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> grid;
+        vector<int>temp;
+        vector<bool>check(nums.size(),false);
+        dfs(grid,nums,check,temp);
+        return grid;
     }
 };
