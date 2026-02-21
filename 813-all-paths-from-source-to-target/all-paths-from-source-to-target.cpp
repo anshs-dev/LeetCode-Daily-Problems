73202f2250afcba4583349430853e8d9b5ccb4df
Ansh Soni <anshs.dev@gmail.com>
2026-02-21 22:21:24 +0530

    Time: 4 ms (61.63%) | Memory: 13.7 MB (90.25%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    void backtrack(vector<vector<int>> &result, vector<int> &temp, vector<vector<int>> &graph, int i,int target){
-        if(i>=graph.size()) return;
-        if(temp.back()==target){
+    void backtrack(vector<vector<int>> &result, vector<vector<int>> &graph, vector<int> &temp, int i, int last){
+        if(temp.back()==last){
             result.push_back(temp);
             return;
-        }
+        } 
         for(int j=0;j<graph[i].size();j++){
             temp.push_back(graph[i][j]);
-            backtrack(result,temp,graph,graph[i][j],target);
+            backtrack(result,graph,temp,graph[i][j],last);
             temp.pop_back();
         }
     }
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        vector<vector<int>> result;
         vector<int>temp;
         temp.push_back(0);
-        backtrack(result,temp,graph,0,graph.size()-1);
+        vector<vector<int>> result;
+        backtrack(result,graph,temp,0,graph.size()-1);
         return result;
     }
 };
