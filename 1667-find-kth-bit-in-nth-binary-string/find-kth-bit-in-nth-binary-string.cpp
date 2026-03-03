3a60eb6f43f0971f3864af61a59d177353b0bf24
Ansh Soni <anshs.dev@gmail.com>
2026-03-03 22:17:29 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (80.15%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
@@ -1,6 +1,11 @@
+// variant
 class Solution {
 public:
     char findKthBit(int n, int k) {
-        return k==1?'0':(__builtin_popcount(k)==1?'1':'0'+1-(findKthBit(n, (1<<(32-__builtin_clz(k)))-k)-'0'));
+        if (k==1) return '0';// base case
+        int sz=bit_width((unsigned)k);
+        int k0=(1<<sz)-k; // reflected number
+        if (k0==k) return '1';// k is a symmetry number
+        return '1'-(findKthBit(n, k0)=='1');
     }
 };
