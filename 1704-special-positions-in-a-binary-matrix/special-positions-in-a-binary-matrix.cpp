5c9831c0a313ba114c6438696451d85bdea04cbe
Ansh Soni <anshs.dev@gmail.com>
2026-03-04 07:47:22 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (96.78%) - LeetSync

---
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool dfs(vector<vector<int>> &mat,int x,int y){
+        for(int i=x-1;i>=0;i--) if(mat[i][y]==1) return false;
+        for(int i=x+1;i<mat.size();i++) if(mat[i][y]==1) return false;
+        for(int i=y-1;i>=0;i--) if(mat[x][i]==1) return false;
+        for(int i=y+1;i<mat[0].size();i++) if(mat[x][i]==1) return false;
+        return true;
+    }
+    int numSpecial(vector<vector<int>>& mat) {
+        int count=0;
+        for(int i=0;i<mat.size();i++){
+            for(int j=0;j<mat[0].size();j++){
+                if(mat[i][j]==1){
+                    count=dfs(mat,i,j)?count+1:count;
+                }
+            }
+        }
+        return count;
+    }
+};
