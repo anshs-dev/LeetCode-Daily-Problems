5f1fb4a8acc2cf8daaa5c2acff132813340260ea
Ansh Soni <anshs.dev@gmail.com>
2026-03-11 15:31:42 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (26.22%) - LeetSync

--- 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
-    int bitwiseComplement(int n) {
-        if(n==0) return 1;
-        long long bits=(int)log2(n)+1;
-        long long mask=(1<<bits)-1;
-        return mask^n;
+    int bitwiseComplement(unsigned n) {
+        if (n == 0) return 1;
+        return ~n & (1 << bit_width(n)) - 1;
     }
 };
