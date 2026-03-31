a73ae9b39399995ba0fb7f4b63b1df17d419edf3
Ansh Soni <anshs.dev@gmail.com>
2026-03-31 08:55:33 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (34.63%) - LeetSync

--- 112-path-sum/path-sum.cpp
+++ 112-path-sum/path-sum.cpp
@@ -1,18 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    bool res=false;
-    void helper(TreeNode* root, int sum){
+    bool check=false;
+    void helper(TreeNode* root,int target){
         if(!root) return;
-        if(!root->left && !root->right && root->val==sum){
-            res=true;
+        if(!root->left && !root->right && root->val==target){ 
+            check=true;
             return;
         }
-        helper(root->left,sum-root->val);
-        helper(root->right,sum-root->val);
+        helper(root->left,target-root->val);
+        helper(root->right,target-root->val);
     }
     bool hasPathSum(TreeNode* root, int targetSum) {
-        if(!root) return false;
         helper(root,targetSum);
-        return res;
+        return check;
     }
 };
