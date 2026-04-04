ff481d00968f19f5adb410b38265a2ed54fca8d3
Ansh Soni <anshs.dev@gmail.com>
2026-04-04 10:00:28 +0530

    Time: 166 ms (28.99%) | Memory: 64.1 MB (11.22%) - LeetSync

---
+++ 301-remove-invalid-parentheses/remove-invalid-parentheses.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool check(string temp){
+        int count=0;
+        for(char x:temp){
+            if(x=='(') count++;
+            else if(x==')') count--;
+            if(count<0) return false;
+        }
+        return count==0;
+    }
+    vector<string> removeInvalidParentheses(string s) {
+        queue<string>q;
+        q.push(s);
+        while(!q.empty()){
+            unordered_set<string>st;
+            while(!q.empty()){
+                st.insert(q.front());
+                q.pop();
+            }
+            for(auto x:st){
+                if(check(x)){
+                    vector<string>res;
+                    for(auto x:st) if(check(x)) res.push_back(x);
+                    return res;
+                }
+            }
+            for(auto x:st){
+                for(int i=0;i<x.size();i++){
+                    q.push(x.substr(0,i)+x.substr(i+1,x.size()));
+                }
+            }
+        }
+        return {};
+    }
+};
