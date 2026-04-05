58e0b0eef33e3950460dbc5bdc08b168c24558b7
Ansh Soni <anshs.dev@gmail.com>
2026-04-05 10:57:02 +0530

    Time: 53 ms (6.53%) | Memory: 39.7 MB (29.10%) - LeetSync

---
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool dfs(vector<int> &arr, int curr, vector<bool> &visited){
+        if(curr<0 || curr>=arr.size() || visited[curr]) return false;
+        if(arr[curr]==0) return true;
+        visited[curr]=true;
+        cout<<curr;
+        return dfs(arr,curr+arr[curr],visited) || dfs(arr,curr-arr[curr],visited);
+    }
+    bool canReach(vector<int>& arr, int start) {
+        vector<bool>visited(arr.size()+1,false);
+        return dfs(arr,start,visited);
+    }
+};
