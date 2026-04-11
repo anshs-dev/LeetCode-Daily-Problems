fc6e177a5f416b253d253a89ad649289f67cb277
Ansh Soni <anshs.dev@gmail.com>
2026-04-11 11:04:06 +0530

    Time: 3 ms (57.04%) | Memory: 32.6 MB (62.38%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,5 +1,24 @@
 class Solution {
 public:
+    int helper(vector<int> &nums, int goal){
+        int left=0,sum=0,res=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            while(left<=i && sum>goal){
+                sum-=nums[left];
+                left++;
+            }
+            res+=(i-left+1);
+        }
+        return res;
+    }
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return helper(nums,goal)-helper(nums,goal-1);
+    }
+};
+/*
+class Solution {
+public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
         long long curr=0,res=0;
         unordered_map<int,int>m;
@@ -12,3 +31,4 @@ public:
         return res;
     }
 };
+*/
