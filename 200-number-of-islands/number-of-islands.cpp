7bb1af77fdac84cd527a194afe57e62500d638ab
Ansh Soni <anshs.dev@gmail.com>
2026-04-15 14:14:53 +0530

    Time: 22 ms (89.07%) | Memory: 16.2 MB (88.84%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
+    void dfs(vector<vector<char>> &grid, int i, int j){
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j]=='0') return;
+        grid[i][j]='0';
+        dfs(grid,i,j+1);
+        dfs(grid,i-1,j);
+        dfs(grid,i+1,j);
+        dfs(grid,i,j-1);
+    }
     int numIslands(vector<vector<char>>& grid) {
         int count=0,m=grid.size(),n=grid[0].size();
-        queue<pair<int,int>>q;
         for(int i=0;i<m;i++) for(int j=0;j<n;j++) if(grid[i][j]=='1'){
             count++;
-            q.push({i,j});
-            while(!q.empty()){
-                int x=q.front().first,y=q.front().second;
-                q.pop();
-                if(x<0 || y<0 || x>=m || y>=n || grid[x][y]=='0') continue;
-                grid[x][y]='0';
-                q.push({x+1,y});
-                q.push({x-1,y});
-                q.push({x,y+1});
-                q.push({x,y-1});
-            }
+            dfs(grid,i,j);
         }
         return count;
     }
