151a638f520e5ba35befd47e38fa86d69e61bcea
Ansh Soni <anshs.dev@gmail.com>
2026-04-17 14:44:50 +0530

    Time: 198 ms (58.98%) | Memory: 124.7 MB (42.24%) - LeetSync

---
+++ 1866-restore-the-array-from-adjacent-pairs/restore-the-array-from-adjacent-pairs.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void dfs(int prev,vector<int> &res, unordered_map<int,vector<int>> &m,int curr){
+        for(int x:m[curr]){
+            if(x==prev) continue;
+            res.push_back(x);
+            prev=curr;
+            curr=x;
+            dfs(prev,res,m,curr);
+            break;
+        }
+    }
+    vector<int> restoreArray(vector<vector<int>>& adjacentPairs) {
+        unordered_map<int,vector<int>> m;
+        for(auto x:adjacentPairs){
+            m[x[0]].push_back(x[1]);
+            m[x[1]].push_back(x[0]);
+        }
+        int first=-1;
+        for(auto x:m) if(x.second.size()==1){
+            first=x.first;
+            break;
+        }
+        vector<int>res;
+        res.push_back(first);
+        //cout<<first;
+        dfs(first,res,m,first);
+        return res;
+    }
+};
