fb5c0d257fbe8641aae354efd48a5bf6f5661d2c
Ansh Soni <anshs.dev@gmail.com>
2026-04-28 19:22:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (32.41%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int first=1,second=2,third=0;
-        if(n==1) return first;
-        if(n==2) return second;
-        n-=2;
-        while(n--){
-            third=first+second;
-            first=second;
-            second=third;
-        }
-        return third;
+        if(n==1) return 1;
+        if(n==2) return 2;
+        vector<int>dp(n+1,0);
+        dp[1]=1,dp[2]=2;
+        for(int i=3;i<=n;i++) dp[i]=dp[i-1]+dp[i-2];
+        return dp[n];
     }
 };
