c19dfcba64132d934cbfcc3f726ae5a31ac5be0a
Ansh Soni <anshs.dev@gmail.com>
2026-05-05 00:01:54 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (92.70%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -6,8 +6,6 @@ public:
                 swap(matrix[i][j],matrix[j][i]);
             }
         }
-        for(int i=0;i<matrix.size();i++){
-            reverse(matrix[i].begin(),matrix[i].end());
-        }
+        for(int i=0;i<matrix.size();i++) reverse(matrix[i].begin(),matrix[i].end());
     }
 };
