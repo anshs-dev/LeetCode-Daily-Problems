5eb6cd20c1eee1ac6fcae0274c5a74897a0321ec
Ansh Soni <anshs.dev@gmail.com>
2026-06-12 16:56:16 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (62.79%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int numrows) {
-        vector<vector<int>>pascal(numrows);
-        for(int i=0;i<numrows;i++){
-        pascal[i].resize(i+1);
-        pascal[i][0]=pascal[i][i]=1;
-        for(int j=1;j<i;j++){
-            pascal[i][j]=pascal[i-1][j-1]+pascal[i-1][j];
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> result;
+        result.push_back(vector<int>(1,1));
+        if(numRows==1) return result;
+            result.push_back(vector<int>(2,1));
+        if(numRows==2) return result;
+        for(int i=3;i<=numRows;i++){
+            vector<int>dp(i,1);
+            for(int j=1;j<i-1;j++){
+                dp[j]=result[i-2][j-1]+result[i-2][j];
+            }
+            result.push_back(dp);
         }
-        }
-        return pascal;
+        return result;
     }
 };
