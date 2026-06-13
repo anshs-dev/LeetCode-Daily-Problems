2a4c83469751b755355c5ce68ef50156f4e3a668
Ansh Soni <anshs.dev@gmail.com>
2026-06-13 09:36:15 +0530

    Time: 43 ms (84.01%) | Memory: 29.1 MB (45.62%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -6,14 +6,17 @@ public:
         for(int i=0;i<nums.size();i++){
             vector<int>temp;
             if(i>0 && nums[i]==nums[i-1]) continue;
-            int sum=nums[i];
-            unordered_set<int>m;
-            for(int j=i+1;j<nums.size();j++){
-                if(m.count(-1*(sum+nums[j]))){
-                    result.push_back({sum,nums[j],-1*(sum+nums[j])});
-                    while(j<nums.size()-1 && nums[j]==nums[j+1]) j++;
+            int sum=nums[i],l=i+1,j=nums.size()-1;
+            while(l<j){
+                if(nums[l]+nums[j]==-(sum)){
+                    result.push_back({nums[i],nums[l],nums[j]});
+                    l++;
+                    j--;
+                while(l<j && nums[l]==nums[l-1]) l++;
+                while(j<nums.size()-1 && j>l && nums[j]==nums[j+1]) j--;
                 }
-                m.insert(nums[j]);
+                else if(nums[l]+nums[j]>-(sum)) j--;
+                else l++;
             }
         }
         return result;
