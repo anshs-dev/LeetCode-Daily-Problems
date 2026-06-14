bfb63ae49e091a99b80e87ac37fb929b91e8317b
Ansh Soni <anshs.dev@gmail.com>
2026-06-15 01:39:48 +0530

    Time: 2341 ms (5.02%) | Memory: 55.7 MB (94.83%) - LeetSync

--- 355-design-twitter/design-twitter.cpp
+++ 355-design-twitter/design-twitter.cpp
@@ -1,59 +1,34 @@
-class Twitter 
-{
-private:
-    map<int, list<pair<int,int>>> tweet;
-    map<int, set<int>> followers;
-    int timestamp;
-
+class Twitter {
 public:
-    Twitter() : timestamp(0)
-    {
+    unordered_map<int,unordered_set<int>>follows;
+    vector<pair<int,int>>v;
+    Twitter() {
         
     }
     
-    void postTweet(int userId, int tweetId)
-    {
-        tweet[userId].push_front({timestamp++,tweetId});
+    void postTweet(int userId, int tweetId) {
+        v.push_back({userId,tweetId});
     }
     
-    vector<int> getNewsFeed(int userId)
-    {
-        // max heap can be used to get the recent 10 tweets.
-        priority_queue<pair<int,int>> recent_tweets;
-        for( const auto& val : tweet[userId])
-        {
-            recent_tweets.push(val);
+    vector<int> getNewsFeed(int userId) {
+        vector<int>result;
+        int i=v.size()-1,tweets=0;
+        while(i>=0 && tweets<10){
+            if(v[i].first==userId || follows[userId].count(v[i].first)) {
+            result.push_back(v[i].second);
+            tweets++;
         }
-
-        // We have to get the tweets from followee also
-        for( const auto& f : followers[userId])
-        {
-            for( const auto& ft : tweet[f])
-            {
-                recent_tweets.push(ft);
-            }
+            i--;
         }
-
-        // Now we need to find the recent 10 tweets.
-        vector<int> tweets;
-        int tweet_count = 0;
-        while(!recent_tweets.empty() && tweet_count < 10)
-        {
-            tweets.push_back(recent_tweets.top().second);
-            recent_tweets.pop();
-            ++tweet_count;
-        }
-        return tweets;
+        return result;
     }
     
-    void follow(int followerId, int followeeId)
-    {
-        followers[followerId].insert(followeeId);
+    void follow(int followerId, int followeeId) {
+        follows[followerId].insert(followeeId);
     }
     
-    void unfollow(int followerId, int followeeId)
-    {
-        followers[followerId].erase(followeeId);
+    void unfollow(int followerId, int followeeId) {
+        follows[followerId].erase(followeeId);
     }
 };
 
