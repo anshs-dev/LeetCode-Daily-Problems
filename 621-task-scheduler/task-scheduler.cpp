93bd75d3ab6d222bffe59f7cdbdb6891fcb2ba07
Ansh Soni <anshs.dev@gmail.com>
2026-06-14 16:25:05 +0530

    Time: 0 ms (100.00%) | Memory: 38.3 MB (82.23%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,25 +1,11 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        priority_queue<pair<int,char>>maxHeap;
-        priority_queue<pair<int,pair<int,char>>,vector<pair<int,pair<int,char>>>,greater<pair<int,pair<int,char>>>>minHeap;
         unordered_map<char,int>m;
-        for(auto x:tasks) m[x]++;
-        for(auto x:m) maxHeap.push({x.second,x.first});
-        int curr=1;
-        while(!maxHeap.empty() || !minHeap.empty()){
-            if(!minHeap.empty() && minHeap.top().first==curr){
-                maxHeap.push({minHeap.top().second.first,minHeap.top().second.second});
-                minHeap.pop();
-            }
-            if(!maxHeap.empty()){
-                int freq=maxHeap.top().first;
-                char c=maxHeap.top().second;
-                maxHeap.pop();
-                if(freq>1) minHeap.push({curr+n+1,{freq-1,c}});    
-            }
-            curr++;
-        }
-        return curr-1;
+        for(char x:tasks) m[x]++;
+        int max_freq=-1,times=0;
+        for(auto x:m) max_freq=max(max_freq,x.second);
+        for(auto x:m) if(x.second==max_freq) times++;
+        return max((int)tasks.size(),(n+1)*(max_freq-1)+times);
     }
 };
