1d539e5b7466187056cdc185e2c6ad65f068a480
Ansh Soni <anshs.dev@gmail.com>
2026-06-18 17:41:25 +0530

    Time: 897 ms (8.66%) | Memory: 115.8 MB (26.08%) - LeetSync

---
+++ 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int res=INT_MIN;
+    struct info{
+        bool isvalid=true;
+        int sum=0;
+        int minsum=INT_MAX;
+        int maxsum=INT_MIN;
+        void print(info curr){
+            cout<<curr.isvalid<<" "
+                <<curr.sum<<" "
+                <<curr.minsum<<" "
+                <<curr.maxsum<<endl;
+        }
+    };
+    info helper(TreeNode* root){
+        if(!root) return {true,0,INT_MAX,INT_MIN};
+        info left=helper(root->left);
+        info right=helper(root->right);
+        res=max({res,left.sum,right.sum});
+        /**
+        cout<<root->val<<" "<<res<<endl;
+        left.print(left);
+        right.print(right);
+        cout<<endl<<endl;
+        **/
+        if(!root->left && !root->right) return {true,root->val,root->val,root->val};
+        if(left.isvalid && right.isvalid && root->val>left.maxsum && root->val<right.minsum) return {true,left.sum+right.sum+root->val,min({left.minsum,root->val,right.minsum}),max({left.maxsum,right.maxsum,root->val})};
+        return {false,max(left.sum,right.sum),min(left.minsum,root->val),max(right.maxsum,root->val)};
+    }
+    int maxSumBST(TreeNode* root) {
+        res=max(res,helper(root).sum);
+        return res<=0?0:res;
+    }
+};
