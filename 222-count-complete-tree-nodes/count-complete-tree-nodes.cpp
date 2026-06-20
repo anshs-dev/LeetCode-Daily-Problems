299ab86a429cdf0430ba890dfd7f7419168ad555
Ansh Soni <anshs.dev@gmail.com>
2026-06-20 18:10:34 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (43.97%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -1,26 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     int lh(TreeNode* root){
-        int len=1;
-        while(root){
-            len++;
-            root=root->left;
+        int temp=0;
+        TreeNode* t=root;
+        while(t){
+            temp++;
+            t=t->left;
         }
-        return len;
+        return temp;
     }
     int rh(TreeNode* root){
-        int len=1;
-        while(root){
-            len++;
-            root=root->right;
+        int temp=0;
+        TreeNode* t=root;
+        while(t){
+            temp++;
+            t=t->right;
         }
-        return len;
+        return temp;
     }
     int countNodes(TreeNode* root) {
-        if(!root) return 0;
-        int l=lh(root->left);
-        int r=rh(root->right);
+        int l=lh(root);
+        int r=rh(root);
         if(l==r) return (1<<l)-1;
-        return countNodes(root->left)+countNodes(root->right)+1;
+        return 1+countNodes(root->left)+countNodes(root->right);
     }
 };
