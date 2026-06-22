ffccdc0a77e32a4cde0c4353ae97b025bcc73107
Ansh Soni <anshs.dev@gmail.com>
2026-06-22 12:12:24 +0530

    Time: 3 ms (26.44%) | Memory: 10.9 MB (27.44%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>> &grid,vector<int> &nums, vector<bool> &check,vector<int>& temp){
-        if(temp.size()==nums.size()){
-            grid.push_back(temp);
+    vector<vector<int>>res;
+    void backtrack(vector<int> &nums, vector<bool> &used, vector<int> &curr){
+        if(curr.size()==nums.size()){
+            res.push_back(curr);
             return;
         }
         for(int i=0;i<nums.size();i++){
-            if(!check[i]){
-                check[i]=true;
-                temp.push_back(nums[i]);
-                dfs(grid,nums,check,temp);
-                check[i]=false;
-                temp.pop_back();
+            if(!used[i]){
+                curr.push_back(nums[i]);
+                used[i]=true;
+                backtrack(nums,used,curr);
+                curr.pop_back();
+                used[i]=false;
             }
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> grid;
-        vector<int>temp;
-        vector<bool>check(nums.size(),false);
-        dfs(grid,nums,check,temp);
-        return grid;
+        vector<bool> used(nums.size(),false);
+        vector<int>curr;
+        backtrack(nums,used,curr);
+        return res;
     }
 };
