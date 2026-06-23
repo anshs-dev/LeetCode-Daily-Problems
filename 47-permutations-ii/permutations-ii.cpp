5f2509d23b974bbb2501a2d378650e48cd4a236b
Ansh Soni <anshs.dev@gmail.com>
2026-06-23 13:59:36 +0530

    Time: 3 ms (61.64%) | Memory: 12.1 MB (51.48%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>> &grid,vector<int> &nums, vector<bool> &check,vector<int>& temp){
-        if(temp.size()==nums.size()){
-            grid.push_back(temp);
+    vector<vector<int>> res;
+    void backtrack(vector<int> &nums, vector<bool> &visited, vector<int> &curr){
+        if(curr.size()==nums.size()){
+            res.push_back(curr);
             return;
         }
         for(int i=0;i<nums.size();i++){
-            if(!check[i]){
-                check[i]=true;
-                temp.push_back(nums[i]);
-                dfs(grid,nums,check,temp);
-                check[i]=false;
-                temp.pop_back();
+            if(!visited[i]){
+                curr.push_back(nums[i]);
+                visited[i]=true;
+                backtrack(nums,visited,curr);
+                visited[i]=false;
+                curr.pop_back();
                 while(i<nums.size()-1 && nums[i]==nums[i+1]) i++;
             }
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        vector<vector<int>> grid;
-        vector<int>temp;
-        vector<bool>check(nums.size(),false);
-        dfs(grid,nums,check,temp);
-        return grid;
+        vector<bool>visited(nums.size(),false);
+        vector<int>v;
+        backtrack(nums,visited,v);
+        return res;
     }
 };
