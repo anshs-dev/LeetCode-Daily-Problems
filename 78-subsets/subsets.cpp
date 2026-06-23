77fefe929c97c8f7d55905dc9f75268712ebeeb2
Ansh Soni <anshs.dev@gmail.com>
2026-06-23 14:32:12 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (59.19%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>> &grid,vector<int> &nums, vector<int> &temp, int i){
-        if(i==nums.size()) {
-            grid.push_back(temp);
+    vector<vector<int>> res;
+    void backtrack(vector<int> &nums, vector<int> &curr, int i){
+        if(i==nums.size()){
+            res.push_back(curr);
             return;
         }
-        temp.push_back(nums[i]);
-        dfs(grid,nums,temp,i+1);
-        temp.pop_back();
-        dfs(grid,nums,temp,i+1);
+            curr.push_back(nums[i]);
+            backtrack(nums,curr,i+1);
+            curr.pop_back();
+            backtrack(nums,curr,i+1);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> grid;
-        vector<int>temp;
-        dfs(grid,nums,temp,0);
-        return grid;
+        vector<int>curr;
+        backtrack(nums,curr,0);
+        return res;
     }
 };
