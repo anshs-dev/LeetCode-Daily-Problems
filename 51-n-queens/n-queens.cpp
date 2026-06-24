3e2e79663dd327c3ee0b0a20eb1057a1e62a7f03
Ansh Soni <anshs.dev@gmail.com>
2026-06-24 13:34:09 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (41.06%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-    void backtrack(vector<vector<string>> &res,vector<string> &v,vector<bool> &col,vector<bool> &diag1, vector<bool> &diag2,int i, int n){
+    vector<vector<string>> v2;
+    void backtrack(int n, vector<string> &grid, vector<bool> &cols, vector<bool> &diag1, vector<bool> &diag2, int i){
         if(i==n){
-            res.push_back(v);
+            v2.push_back(grid);
             return;
         }
-        for(int k=0;k<n;k++){
-            if(!col[k] && !diag1[i+k] && !diag2[i+n-k]){
-                v[i][k]='Q';
-                col[k]=true;
-                diag1[i+k]=true;
-                diag2[i+n-k]=true;
-                backtrack(res,v,col,diag1,diag2,i+1,n);
-                v[i][k]='.';
-                col[k]=false;
-                diag1[i+k]=false;
-                diag2[i+n-k]=false;
+        for(int j=0;j<n;j++){
+            if(!cols[j] && !diag1[i+j] && !diag2[n+i-j-1]){
+                grid[i][j]='Q';
+                cols[j]=true;
+                diag1[i+j]=true;
+                diag2[n+i-j-1]=true;
+                backtrack(n,grid,cols,diag1,diag2,i+1);
+                grid[i][j]='.';
+                cols[j]=false;
+                diag1[i+j]=false;
+                diag2[n+i-j-1]=false;
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> res;
         string temp="";
-        vector<string>v;
         for(int i=0;i<n;i++) temp+=".";
+        vector<string>v;
         for(int i=0;i<n;i++) v.push_back(temp);
-        vector<bool> col(n,false),diag1(2*n-1,false),diag2(2*n-1,false);
-        backtrack(res,v,col,diag1,diag2,0,n);
-        return res;
+        vector<bool> cols(n,false),diag1(2*n-1,false),diag2(2*n-1,false);
+        backtrack(n,v,cols,diag1,diag2,0);
+        return v2;
     }
 };
