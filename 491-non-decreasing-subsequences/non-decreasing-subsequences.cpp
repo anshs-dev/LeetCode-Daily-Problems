e6790ac6a62faa9ba98d4e6739e1d278654198c1
Ansh Soni <anshs.dev@gmail.com>
2026-06-24 14:18:54 +0530

    Time: 30 ms (67.08%) | Memory: 29.9 MB (44.80%) - LeetSync

--- 491-non-decreasing-subsequences/non-decreasing-subsequences.cpp
+++ 491-non-decreasing-subsequences/non-decreasing-subsequences.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>> &res, vector<int> &curr, vector<int> &nums, int j){
-        if(curr.size()>=2) res.push_back(curr);
+    vector<vector<int>> res;
+    void dfs(vector<int> &nums, vector<int> &curr, int i){
+        if(curr.size()>1) res.push_back(curr);
+        if(i>=nums.size()) return;
         unordered_set<int>st;
-        for(int i=j;i<nums.size();i++){
-            if(st.count(nums[i])) continue;
-            st.insert(nums[i]);
-            if(curr.empty() || (curr.back()<=nums[i])){
-                curr.push_back(nums[i]);
-                dfs(res,curr,nums,i+1);
+        for(int j=i;j<nums.size();j++){
+            if(st.count(nums[j])) continue;
+            if(curr.empty() || nums[j]>=curr.back()){
+                curr.push_back(nums[j]);
+                st.insert(nums[j]);
+                dfs(nums,curr,j+1);
                 curr.pop_back();
             }
         }
     }
     vector<vector<int>> findSubsequences(vector<int>& nums) {
-        vector<vector<int>> res;
         vector<int>curr;
-        dfs(res,curr,nums,0);
+        dfs(nums,curr,0);
         return res;
     }
 };
