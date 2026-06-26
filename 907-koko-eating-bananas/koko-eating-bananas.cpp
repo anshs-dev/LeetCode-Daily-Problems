f1103d20a085a2f74603c4c1c463daac37020f7c
Ansh Soni <anshs.dev@gmail.com>
2026-06-26 15:45:24 +0530

    Time: 4 ms (87.56%) | Memory: 22.9 MB (46.64%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    bool valid(vector<int> &piles, int mid,int h){
-        int time=0;
-        for(int x:piles) time+=((x+mid-1)/mid);
-        return time<=h;
+    bool isvalid(vector<int> &piles, int h, int speed){
+        unsigned long long count=0;
+        for(int x:piles){
+            count+=((x+speed-1)/speed);
+        }
+        //cout<<speed<<" "<<count<<" "<<(speed>=h)<<endl;
+        return count<=h;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-       // sort(piles.begin(),piles.end());
-        int low=1,high=*max_element(piles.begin(),piles.end());
-        while(low<high){
-            int mid=low+(high-low)/2;
-            if(valid(piles,mid,h)) high=mid;
-            else low=mid+1;
+        int right=*max_element(piles.begin(),piles.end()),left=1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(isvalid(piles,h,mid)) right=mid-1;
+            else left=mid+1;
         }
-        return high;
+        return left;
     }
 };
