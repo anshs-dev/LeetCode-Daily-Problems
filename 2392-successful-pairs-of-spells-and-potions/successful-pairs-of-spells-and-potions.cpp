52e1d2e3c859a0ea63b3807f127a79d604b80e90
Ansh Soni <anshs.dev@gmail.com>
2026-06-27 15:28:00 +0530

    Time: 49 ms (25.69%) | Memory: 128.4 MB (29.15%) - LeetSync

--- 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.cpp
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    void helper(int num,vector<int> &potions,vector<int>& result,long long success){
-        int low=0,high=potions.size()-1;
-        auto it=lower_bound(potions.begin(),potions.end(),success,[&](long long n,long long target){
-            return n*num*1LL<target;
-        });
-        result.push_back(it!=potions.end()?potions.size()-(it-potions.begin()):0);
-    }
-    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success){
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
         sort(potions.begin(),potions.end());
-        vector<int>result;
-        for(int x:spells) helper(x,potions,result,success);
-        return result;
+        vector<int>res;
+        for( long long x:spells){
+             long long low=0,high=potions.size()-1;
+            while(low<=high){
+                 long long mid=low+(high-low)/2;
+                if(x*potions[mid]>=success) high=mid-1;
+                else low=mid+1;
+            }
+            res.push_back(potions.size()-low);
+        }
+        return res;
     }
 };
