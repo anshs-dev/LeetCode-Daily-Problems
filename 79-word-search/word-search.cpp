c93bc84162d6720f0d3fec17f1e8a94135d26257
Ansh Soni <anshs.dev@gmail.com>
2026-06-29 14:05:55 +0530

    Time: 179 ms (94.11%) | Memory: 10.5 MB (82.04%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    bool dfs(vector<vector<char>>& board, string word, vector<vector<bool>> &visited, int i, int j, int m, int n, int k){
-        if(k==word.size()) return true;
-        if(i<0 || i>=m || j<0 || j>=n || visited[i][j] || board[i][j]!=word[k]) return false;
-        visited[i][j]=true;
-        bool a=dfs(board,word,visited,i+1,j,m,n,k+1); 
-        bool b=dfs(board,word,visited,i,j+1,m,n,k+1);  
-        bool c=dfs(board,word,visited,i-1,j,m,n,k+1);  
-        bool d=dfs(board,word,visited,i,j-1,m,n,k+1);
-        visited[i][j]=false;
+    bool backtrack(vector<vector<char>> &board, int m, int n, string &word, int i, int j, int k){
+        if(k>=word.size()) return true;
+        if(i<0 || j<0 || i>=m || j>=n || board[i][j]=='*' || board[i][j]!=word[k]) return false;
+        char temp=board[i][j];
+        board[i][j]='*';
+        bool a=backtrack(board,m,n,word,i+1,j,k+1);
+        bool b=backtrack(board,m,n,word,i,j+1,k+1);
+        bool c=backtrack(board,m,n,word,i-1,j,k+1);
+        bool d=backtrack(board,m,n,word,i,j-1,k+1);
+        board[i][j]=temp;
         return a || b || c || d;
     }
     bool exist(vector<vector<char>>& board, string word) {
         int m=board.size(),n=board[0].size();
-        for(int i=0;i<m;i++){ 
+        for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                vector<vector<bool>> visited(m,vector<bool>(n,false));
-                if(board[i][j]==word[0] && dfs(board,word,visited,i,j,m,n,0)) return true;
+                if(board[i][j]==word[0] && backtrack(board,m,n,word,i,j,0)) return true;
             }
         }
         return false;
