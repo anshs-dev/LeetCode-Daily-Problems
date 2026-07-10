ec4ea8b858b08ec3c563ff5998743fadf3fbb9cf
Ansh Soni <anshs.dev@gmail.com>
2026-07-10 07:09:15 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (46.87%) - LeetSync

---
+++ 275-h-index-ii/h-index-ii.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int low=0,high=citations.size()-1,n=citations.size();
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(citations[mid]>=n-mid) high=mid-1;
+            else low=mid+1;
+        }
+        return n-low;
+    }
+};
