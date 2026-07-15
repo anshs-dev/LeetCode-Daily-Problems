7f6a6fb00649b5e7db015384583880f31f252ba6
Ansh Soni <anshs.dev@gmail.com>
2026-07-15 16:31:36 +0530

    Time: 68 ms (5.18%) | Memory: 169.4 MB (35.45%) - LeetSync

---
+++ 4351-minimum-total-cost-to-process-all-elements/minimum-total-cost-to-process-all-elements.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& nums, int k) {
+        const int mod=1e9+7;
+        sort(nums.begin(),nums.end());
+        int count=0,curr=k,start=1;
+        for(int x:nums){
+            if(curr<x){
+                int temp=(x+k-curr-1)/k;
+                curr+=(k*temp);
+                curr-=x;
+                count = (count + (1LL * temp * (start * 2LL + temp - 1) / 2) % mod) % mod;
+                start=(start+temp)%mod;
+            }
+            else curr-=x;
+        }
+        return count;
+    }
+};
